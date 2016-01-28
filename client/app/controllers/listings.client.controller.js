1e524c74de7d85bf44a8a0d6c71444b95b4fa435
Amy <amy.devries@ufl.edu>
2016-01-28 18:05:11 -0500

    Got delete listing to work
    Added code to make deleting a listing functional. Still trying to figure
    out the edit.

--- client/app/controllers/listings.client.controller.js
+++ client/app/controllers/listings.client.controller.js
@@ -79,13 +79,48 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
         successfully finished, navigate back to the 'listing.list' state using $state.go(). If an error 
         occurs, pass it to $scope.error. 
        */
+       $scope.error = null;
+
+      /* 
+        Check that the form is valid. (https://github.com/paulyoder/angular-bootstrap-show-errors)
+       */
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'articleForm');
+
+        return false;
+      }
+
+      /* Create the listing object */
+      var listing = {
+        name: $scope.name, 
+        code: $scope.code, 
+        address: $scope.address
+      };
+
+      /* Save the article using the Listings factory */
+      Listings.update(listingId, listing)
+              .then(function(response) {
+                //if the object is successfully saved redirect back to the list page
+                $state.go('listings.list', { successMessage: 'Listing succesfully saved!' });
+              }, function(error) {
+                //otherwise display the error
+                $scope.error = 'Unable to save listing!\n' + error;
+              });
     };
 
-    $scope.remove = function() {
+    $scope.remove = function(listingId) {
       /*
         Implement the remove function. If the removal is successful, navigate back to 'listing.list'. Otherwise, 
         display the error. 
        */
+       Listings.delete(listingId)
+          .then(function(response) {
+                //if the object is successfully saved redirect back to the list page
+                $state.go('listings.list', { successMessage: 'Listing succesfully deleted!' });
+              }, function(error) {
+                //otherwise display the error
+                $scope.error = 'Unable to delete listing!\n' + error;
+              });
     };
 
     /* Bind the success message to the scope if it exists as part of the current state */
